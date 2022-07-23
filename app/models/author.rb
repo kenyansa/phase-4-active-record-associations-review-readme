187af15f0c363c8f4bb455408248f02dd9500b55
Kenyansa <kenyansajustine@gmail.com>
2022-07-23 19:58:31 +0300

    completed codealong exercise

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ApplicationRecord
+    has_many :posts 
+
+    #adding another one
+    has_one :profile 
+end
+
+class Profile < ApplicationRecord
+    #adding stuff
+    belongs_to :author
 end
